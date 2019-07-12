4fa0fd2a23163b22574d68f7cf6e13ed61d4510e
Danny McCormick <damccorm@microsoft.com>
2019-07-11 22:57:54 -0400

    Add problem matcher (#4)

--- src/setup-java.ts
+++ src/setup-java.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as installer from './installer';
+import * as path from 'path';
 
 async function run() {
   try {
@@ -8,6 +9,9 @@ async function run() {
     const jdkFile = core.getInput('jdkFile', {required: true});
 
     await installer.getJava(version, arch, jdkFile);
+
+    const matchersPath = path.join(__dirname, '..', '.github');
+    console.log(`##[add-matcher]${path.join(matchersPath, 'java.json')}`);
   } catch (error) {
     core.setFailed(error.message);
   }
