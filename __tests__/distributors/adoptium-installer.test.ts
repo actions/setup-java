330fe63577717f626dd19f0242956a3c4ab6ccef
Maxim Lobanov <v-malob@microsoft.com>
2021-03-08 18:34:14 +0300

    Add unit tests and e2e tests

---
+++ __tests__/distributors/adoptium-installer.test.ts
@@ -0,0 +1,146 @@
+import { HttpClient } from '@actions/http-client';
+
+import * as semver from 'semver';
+
+import { AdoptiumDistribution } from '../../src/distributions/adoptium/installer';
+import { JavaInstallerOptions } from '../../src/distributions/base-models';
+
+let manifestData = require('../data/adoptium.json') as [];
+
+describe('getAvailableVersions', () => {
+  let spyHttpClient: jest.SpyInstance;
+
+  beforeEach(() => {
+    spyHttpClient = jest.spyOn(HttpClient.prototype, 'getJson');
+    spyHttpClient.mockReturnValue({
+      statusCode: 200,
+      headers: {},
+      result: []
+    });
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  it.each([
+    [
+      { version: '11', arch: 'x64', packageType: 'jdk' },
+      'os=mac&architecture=x64&image_type=jdk&release_type=ga&page_size=20&page=0'
+    ],
+    [
+      { version: '11', arch: 'x86', packageType: 'jdk' },
+      'os=mac&architecture=x86&image_type=jdk&release_type=ga&page_size=20&page=0'
+    ],
+    [
+      { version: '11', arch: 'x64', packageType: 'jre' },
+      'os=mac&architecture=x64&image_type=jre&release_type=ga&page_size=20&page=0'
+    ],
+    [
+      { version: '11-ea', arch: 'x64', packageType: 'jdk' },
+      'os=mac&architecture=x64&image_type=jdk&release_type=ea&page_size=20&page=0'
+    ]
+  ])(
+    'build correct url for %s',
+    async (installerOptions: JavaInstallerOptions, expectedParameters) => {
+      const distribution = new AdoptiumDistribution(installerOptions);
+      const baseUrl = 'https://api.adoptopenjdk.net/v3/assets/version/%5B1.0,100.0%5D';
+      const expectedUrl = `${baseUrl}?project=jdk&vendor=adoptopenjdk&heap_size=normal&jvm_impl=hotspot&sort_method=DEFAULT&sort_order=DESC&${expectedParameters}`;
+      distribution['getPlatformOption'] = () => 'mac';
+
+      await distribution['getAvailableVersions']();
+
+      expect(spyHttpClient.mock.calls).toHaveLength(1);
+      expect(spyHttpClient.mock.calls[0][0]).toBe(expectedUrl);
+    }
+  );
+
+  it('load available versions', async () => {
+    spyHttpClient = jest.spyOn(HttpClient.prototype, 'getJson');
+    spyHttpClient
+      .mockReturnValueOnce({
+        statusCode: 200,
+        headers: {},
+        result: manifestData
+      })
+      .mockReturnValueOnce({
+        statusCode: 200,
+        headers: {},
+        result: manifestData
+      })
+      .mockReturnValueOnce({
+        statusCode: 200,
+        headers: {},
+        result: []
+      });
+
+    const distribution = new AdoptiumDistribution({
+      version: '11',
+      arch: 'x64',
+      packageType: 'jdk'
+    });
+    const availableVersions = await distribution['getAvailableVersions']();
+    expect(availableVersions).not.toBeNull();
+    expect(availableVersions.length).toBe(manifestData.length * 2);
+  });
+});
+
+describe('findPackageForDownload', () => {
+  it.each([
+    ['9', '9.0.7+10'],
+    ['15', '15.0.2+7'],
+    ['15.0', '15.0.2+7'],
+    ['15.0.2', '15.0.2+7'],
+    ['15.0.1', '15.0.1+9.1'],
+    ['11.x', '11.0.10+9'],
+    ['x', '15.0.2+7'],
+    ['12', '12.0.2+10.3'] // make sure that '12.0.2+10.1', '12.0.2+10.3', '12.0.2+10.2' are sorted correctly
+  ])('version is resolved correctly %s -> %s', async (input, expected) => {
+    const distribution = new AdoptiumDistribution({
+      version: '11',
+      arch: 'x64',
+      packageType: 'jdk'
+    });
+    distribution['getAvailableVersions'] = async () => manifestData;
+    const resolvedVersion = await distribution['findPackageForDownload'](new semver.Range(input));
+    expect(resolvedVersion.version).toBe(expected);
+  });
+
+  it('version is found but binaries list is empty', async () => {
+    const distribution = new AdoptiumDistribution({
+      version: '11',
+      arch: 'x64',
+      packageType: 'jdk'
+    });
+    distribution['getAvailableVersions'] = async () => manifestData;
+    await expect(
+      distribution['findPackageForDownload'](new semver.Range('9.0.8'))
+    ).rejects.toThrowError(/Could not find satisfied version for SemVer */);
+  });
+
+  it('version is not found', async () => {
+    const distribution = new AdoptiumDistribution({
+      version: '11',
+      arch: 'x64',
+      packageType: 'jdk'
+    });
+    distribution['getAvailableVersions'] = async () => manifestData;
+    await expect(
+      distribution['findPackageForDownload'](new semver.Range('7.x'))
+    ).rejects.toThrowError(/Could not find satisfied version for SemVer */);
+  });
+
+  it('version list is empty', async () => {
+    const distribution = new AdoptiumDistribution({
+      version: '11',
+      arch: 'x64',
+      packageType: 'jdk'
+    });
+    distribution['getAvailableVersions'] = async () => [];
+    await expect(
+      distribution['findPackageForDownload'](new semver.Range('11'))
+    ).rejects.toThrowError(/Could not find satisfied version for SemVer */);
+  });
+});
