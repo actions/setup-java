408d6f3eea436fd353321fd91a7fbddcd114331d
Maxim Lobanov <v-malob@microsoft.com>
2021-03-10 19:54:50 +0300

    handle 4th digit

--- src/util.ts
+++ src/util.ts
@@ -1,10 +1,13 @@
 import os from 'os';
 import path from 'path';
+import * as fs from 'fs';
+import * as semver from 'semver';
 
 import * as tc from '@actions/tool-cache';
-
 export function getTempDir() {
-  return process.env['RUNNER_TEMP'] || os.tmpdir();
+  let tempDirectory = process.env['RUNNER_TEMP'] || os.tmpdir();
+
+  return tempDirectory;
 }
 
 export function getVersionFromToolcachePath(toolPath: string) {
@@ -36,4 +39,28 @@ export async function extractJdkFile(toolPath: string, extension?: string) {
 
 export function getDownloadArchiveExtension() {
   return process.platform === 'win32' ? 'zip' : 'tar.gz';
+}
+
+export function isVersionSatisfies(range: string, version: string): boolean {
+  if (semver.valid(range)) {
+    // if full version with build digit is provided as a range (such as '1.2.3+4')
+    // we should check for exact equal via compareBuild
+    // since semver.satisfies doesn't handle 4th digit
+    const semRange = semver.parse(range);
+    if (semRange && semRange.build?.length > 0) {
+      return semver.compareBuild(range, version) === 0;
+    }
+  }
+
+  return semver.satisfies(version, range);
+}
+
+export function getToolcachePath(toolName: string, version: string, architecture: string) {
+  const toolcacheRoot = process.env['RUNNER_TOOL_CACHE'] ?? '';
+  const fullPath = path.join(toolcacheRoot, toolName, version, architecture);
+  if (fs.existsSync(fullPath)) {
+    return fullPath;
+  }
+
+  return null;
 }
