4e6af5a2d7d7c1d4da9ee0ba337d9100d60e0bf0
Maxim Lobanov <maxim-lobanov@github.com>
2021-03-19 17:25:37 +0300

    Update src/setup-java.ts
    Co-authored-by: Konrad Pabjan <konradpabjan@github.com>

--- src/setup-java.ts
+++ src/setup-java.ts
@@ -57,7 +57,7 @@ async function run() {
     }
 
     core.warning(
-      `All setup-java actions pinned to 'main' branch will fail on 5th April 2021. Please explicitly reference your action to the 'v1' tag ('actions/setup-java@v1') to avoid build failures. Find more details in https://github.com/actions/setup-java/issues/137`
+      `All setup-java actions pinned to the 'main' branch will fail on April 5th 2021. Please explicitly reference your action with the 'v1' tag ('actions/setup-java@v1') to avoid build failures. Find more details at https://github.com/actions/setup-java/issues/137`
     );
   } catch (error) {
     core.setFailed(error.message);
