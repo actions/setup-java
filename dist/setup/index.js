61c179a6ef8ea7bae7ab53f9e1e62ca9b1caebfa
Pavel Krulec <70140667+pavelkrulec-ext96968@users.noreply.github.com>
2021-09-14 14:04:11 +0200

    Bump http-client version (actions#221) (#222)

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -39653,7 +39653,9 @@ class HttpClient {
                 maxSockets: maxSockets,
                 keepAlive: this._keepAlive,
                 proxy: {
-                    proxyAuth: `${proxyUrl.username}:${proxyUrl.password}`,
+                    ...((proxyUrl.username || proxyUrl.password) && {
+                        proxyAuth: `${proxyUrl.username}:${proxyUrl.password}`
+                    }),
                     host: proxyUrl.hostname,
                     port: proxyUrl.port
                 }
