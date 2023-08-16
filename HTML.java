98c35603e36caf329ff449103b4642ae219e5109
phonrathana.github.io <127619322+phonRathana@users.noreply.github.com>
2023-08-17 04:31:14 +0700

    Update index.html

--- HTML.java
+++ HTML.java
@@ -1,121 +1,55 @@
-@google.Search
 <html>
   <head>
-     ("hello the world ")    
-     <meta charset="utf-8">
-     <title>Rathana Phone Shop1</title>
-     <meta name="google-site-verification" content="6o3mfq88FPLGRnIKr5-LYJCU0HEBlaS37S48w34IWe0" />
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet">
-     /* Style your website here */
+   <meta http-equiv="Content-type" content="text/html; charset=utf-8">   
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="icon" href="img/My.photo.png">       
+    <title>Rathana.phone.shop1</title>
+    <style>
       body {
-         <link rel="stylesheet" href="css/custom.css"> 
-   </head> 
-   <body> 
-     <div class="top-bar"> 
-       <div class="bar-left"> 
-         <div class="menu"> 
-           <a href="">About</a> 
-           <a href="">Shop</a> 
-         </div> 
-       </div> 
-       <div class="bar-right"> 
-         <div class="menu"> 
-           <a href="">Gmail</a> 
-           <a href="">Images</a> 
-         </div> 
-         <a href="" class="app-toggle"> 
-           <i class="fa fa-ellipsis-h"></i> 
-         </a> 
-         <a href="" class="profile-trigger"> 
-           <img src="images/profile.jpg"> 
-         </a> 
-       </div> 
-     </div> 
-     <div class="content"> 
-       <div class="search-wrapper"> 
-         <a href="" class="search-logo">
-           <img src="images/logo-Rathana phone shop1-"> 
-         </a> 
-         <div class="search-bar"> 
-           <div class="search-icon"> 
-             <i class="fa fa-search"></i> 
-           </div> 
-           <input type="text"> 
-           <div class="search-icon right"> 
-             <i class="fa fa-microphone"></i> 
-           </div> 
-         </div>
-        </a> 
-         <div class="search-bar"> 
-           <div class="search-icon"> 
-             <i class="fa fa-search"></i> 
-           </div> 
-           <input type="text"> 
-           <div class="search-icon right"> 
-             <i class="fa fa-microphone"></i> 
-           </div> 
-         </div> 
-         <div class="search-buttons"> 
-           <a href="">Rathana phone Shop1 Search</a> 
-           <a href="">I'm Feeling Lucky</a> 
-         </div>
-         <p class="search-info"> 
-           This is a search bar. <a href="">Random Link</a> 
-        </p> 
-      </div> 
-    </div> 
-    <div class="bottom-bar"> 
-       <div class="bar-left"> 
-         <div class="menu"> 
-          <a href="">Advertising</a> 
-          <a href="">Business</a> 
-          <a href="">How Search Works</a> 
-         </div> 
-       </div>
+        margin: 10px;
+      }
+      #btn {
+        text-decoration: underline;
+        cursor: pointer;
+      }
+
+      #My.photo_img {
+        height: 500px;
+        width: 500px;
       }
     </style>
   </head>
   <body>
-    <header>
-      <h1>Rathana Phone Shop1</h1>
-    </header>
-    <nav>
-       <a href="#">Home</a>
-       <a href="#">About</a>
-       <a href="#">Phones</a>
-       <a href="#">Accessories</a>
-       <a href="#">Contact</a>
-     </nav>
-     <main>
-      <h2>Welcome to Rathana Phone Shop1</h2>
-      <p>We offer a wide selection of the latest smartphones and accessories from all the major brands. Our knowledgeable staff can help you find the  perfect device to fit your needs and budget.</p>
-      <h3>Featured Products</h3>
-      <ul>
-        <h3>Contact km</h3>
-      <p>Visit us at:</p>
-      <p>home14 St5 Sangkat Dangkor.</p>
-      <p>Anytown, Phnom Penh </p>
-      <p>Phone: (085883923) </p>
-      <p>Email: info@rathanaphone29@gmail.com</p>
-    </main>
-    <footer>
-      <p>&copy; 2023 Rathana Phone Shop1 All rights reserved.</p>
-        <li>Apple iPhone 12 Pro</li>
-        <li>OnePlus 9 Pro</li>
-      </ul>
-         </div> 
-       </div> 
-       <div class="bar-right"> 
-         <div class="menu"> 
-           <a href="">pulled</a> 
-           <a href="">Terms</a> 
-           <a href="">Settings</a> 
-         </div> 
-       </div> 
-     </div> 
-   </body> 
- </html>
-
+    <h1>Applicants</h1>
+      <form>
+    <hr>
+      <p><em>Select an applicant from the list to see their details</em></p>
+      <select id="select"> 
+      <option disabled selected value> -- select -- </option>
+      </select>
+      &nbsp;<a id="btn">Display</a>
+    </form>
+     <hr>
+    </form>
+    <hr>
+     <p><strong>Name:</strong> <span id ="">-</span></p>
+     <p><strong>tell:</strong> <span id="">-</span></p>
+     <p><strong>address:</strong> <span id="">-</span></p>
+     <p><strong>Gmail:</strong>
+     <p><strong>sipping:</strong>
+     <span id="<devLanguage">-</span></p>
+     <p><strong>My.photo avatar</strong></p>
+    </form>
+     <hr> 
+  </div>
+    <Link href="http://httpsrathaphon.github.io/website.info/">
+     <a href="http://httpsrathaphon.github.io./website.info/" class="logo logo-img-1x"> 
+       <img width="500px" height="500px" title="Rathana.phone.shop1" alt=""src=https://avatars.githubusercontent.com/u/127619322?s=48&amp;v=4jpeg ")no-repeat; 
+         </a>
+       </footer> 
+     </body> 
+  </html>
+   
+      
 
 
