6bbc741f104f7f2d08f0c8711c78a36f3e888cfd
mahabaleshwars <mahabaleshwar.s@infosys.com>
2024-02-26 12:55:52 +0530

    added changes to distribution files

--- src/distributions/microsoft/installer.ts
+++ src/distributions/microsoft/installer.ts
@@ -26,7 +26,20 @@ export class MicrosoftDistributions extends JavaBase {
     core.info(
       `Downloading Java ${javaRelease.version} (${this.distribution}) from ${javaRelease.url} ...`
     );
-    const javaArchivePath = await tc.downloadTool(javaRelease.url);
+    let javaArchivePath = await tc.downloadTool(javaRelease.url);
+    // Rename archive to add extension because after downloading
+    // archive does not contain extension type and it leads to some issues
+    // on Windows runners without PowerShell Core.
+    //
+    // For default PowerShell Windows it should contain extension type to unpack it.
+    if (
+      process.platform === 'win32' &&
+      (this.architecture === 'arm64' || this.architecture === 'aarch64')
+    ) {
+      const javaArchivePathRenamed = `${javaArchivePath}.zip`;
+      await fs.renameSync(javaArchivePath, javaArchivePathRenamed);
+      javaArchivePath = javaArchivePathRenamed;
+    }
 
     core.info(`Extracting Java archive...`);
     const extension = getDownloadArchiveExtension();
