23f648d4044d48237fe254ac06a4d7ca4d4abe1b
mahabaleshwars <mahabaleshwar.s@infosys.com>
2024-03-08 10:33:12 +0530

    updated regex to accept early versions

--- src/util.ts
+++ src/util.ts
@@ -120,7 +120,7 @@ export function getVersionFromFileContent(
 ): string | null {
   let javaVersionRegExp: RegExp;
   if (versionFile == '.tool-versions') {
-    javaVersionRegExp = /^java\s(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/m;
+    javaVersionRegExp = /^java\s+(?:\S+-)?v?(?<version>[^\s-]+(?:-\S+)?)$/m;
   } else if (versionFile == '.java-version') {
     javaVersionRegExp = /(?<version>(?<=(^|\s|-))(\d+\S*))(\s|$)/;
   } else {
